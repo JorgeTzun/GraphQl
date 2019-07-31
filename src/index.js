36e7a004dce1b7c19bea420147fdaad959ae773b
JorgeTzun <jorge.tzun@outlook.com>
2019-07-31 18:28:30 -0500

    Implementa directives

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const {GraphQLServer} = require ('graphql-yoga');
 const {importSchema} = require('graphql-import');
 const mongoose = require('../config/conexion');
+const AuthDirective =  require('./resolvers/directive')
+const verifyToken  = require('./utils/jwt');
 const typeDefs  =  importSchema('./src/schema.graphql');
+const { makeExecutableSchema } = require('graphql-tools');
 const {getAllEmpresa, getFilterEmpresa, getEmpresa} =  require('../src/resolvers/Querys');
 const {createEmpresa, createUser, login} =  require('../src/resolvers/Mutations');
 
@@ -19,6 +22,14 @@ const {createEmpresa, createUser, login} =  require('../src/resolvers/Mutations'
         login
      }
 }
+
+const Schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+    schemaDirectives: {
+       auth: AuthDirective
+    }
+});
 
 const server = new GraphQLServer({ typeDefs, resolvers})
 server.start(() => console.log('Server is running on localhost:4000'))
